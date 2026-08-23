7def86953709aa46348dfe1c475aa4b8a17141e4
amilkanthwarshriya-eng <amilkanthwarshriya@gmail.com>
2026-08-23 20:13:33 +0530

    Time: 0 ms (100.00%) | Memory: 15 MB (94.97%) - LeetSync

--- 103-binary-tree-zigzag-level-order-traversal/binary-tree-zigzag-level-order-traversal.cpp
+++ 103-binary-tree-zigzag-level-order-traversal/binary-tree-zigzag-level-order-traversal.cpp
@@ -18,33 +18,26 @@ public:
         queue<TreeNode*> q;
 
         q.push(root);
-        int flag = 1;
+        bool flag = true;
 
         while(!q.empty())
-        {
-            vector<int> level;
+        { 
             int size = q.size();
+            vector<int> level(size);
 
             for(int i=0;i<size;i++)
             {
                 TreeNode* temp  = q.front();
                 q.pop();
 
-                
+                int index = flag ? i : size-1-i;
+                level[index] = temp->val;
+
                 if(temp->left) q.push(temp->left);
                 if(temp->right) q.push(temp->right);
-                
-
-                level.push_back(temp->val);
-
             }
-            if(flag==0)
-            {
-               reverse(level.begin(),level.end()); 
-               flag = 1;
-            }else{
-                flag = 0;
-            }
+
+            flag = !flag;
             ans.push_back(level);
 
         }
